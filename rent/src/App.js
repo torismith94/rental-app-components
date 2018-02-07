5f703258c50fc3c7df50c923004890a8aba73703
torismith <victoriaashleysmith@gmail.com>
2018-02-06 19:05:43 -0800

    app.js main page

--- rent/src/App.js
+++ rent/src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to RecreEntals.</h1>
+          <h2>A place to rent the small stuff (or the big stuff).</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
