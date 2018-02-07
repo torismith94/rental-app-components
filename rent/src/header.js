3016cfe3bbd95e27e1b39446854f4ff69936becc
torismith <victoriaashleysmith@gmail.com>
2018-02-06 21:20:13 -0800

    header

--- rent/src/header.js
+++ rent/src/header.js
@@ -1,5 +1,15 @@
 // burger bar, logo, and "Add item for rent" 
 // search bar that carries over to all pages except home.js
-
 import React, { Component } from 'react';
 import './App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className=".col-5"> 
+      </div>
+    );
+  }
+}
+
+export default Header;
