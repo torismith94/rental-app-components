3f5e02d268b5330003545a2fbf42958bcb2ee96f
drj613 <drj613@gmail.com>
2018-02-08 20:02:51 -0800

    Beginning of splash styling

--- rent/src/splash.js
+++ rent/src/splash.js
@@ -5,3 +5,25 @@
 
 import React, { Component } from 'react';
 import './App.css';
+
+class Splash extends Component {
+    render() {
+        return(
+            <div className="container-fluid" id="splash">
+                <div className = "row">
+                    <div className = "col-2 col-sm-12">
+                        <div className="logo">
+                            <img alt="logo" href="#" />
+                        </div>
+                    </div>
+                </div>
+                <div className = "row">
+                    
+                </div>
+                <div className = "row">
+
+                </div>
+            </div>
+        );
+    }
+}
