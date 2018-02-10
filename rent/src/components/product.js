e296e21d57b7e63fb9c3de67497f8f55c7a68735
drj613 <drj613@gmail.com>
2018-02-10 12:34:32 -0800

    product styling

--- rent/src/components/product.js
+++ rent/src/components/product.js
@@ -5,7 +5,7 @@ class Product extends Component{
     render(){
         return(
             <div className="col-xs-6 col-s-6 col-md-4 col-lg-3 product">
-                <a href="https://placeholder.com"><img src="http://via.placeholder.com/150x150" /></a>
+                <a href="#"><img src="http://via.placeholder.com/150x150" /></a>
             </div>
         );
     }
